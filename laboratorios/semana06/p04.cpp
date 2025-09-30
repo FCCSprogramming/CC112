829b0fdc3226687210390053d9b02795653ab4b2
cc112FC <achulluncuyr@uni.edu.pe>
2025-09-30 15:40:43 -0500

    ejercicios pd6

--- laboratorios/semana06/p04.cpp
+++ laboratorios/semana06/p04.cpp
@@ -11,18 +11,59 @@ Salida: Un mensaje que indique si las cadenas son anagramas o no.
 */
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-void ordenaCadena(){
-
+void ordenaCadena(char* cadena){
+    int n = strlen(cadena);
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (*(cadena + j) > *(cadena + j + 1)) {
+                swap(*(cadena + j), *(cadena + j + 1));
+            }
+        }
+    }
 }
 
-
 bool esAnagrama(char* cad1, char* cad2){
+    if (strlen(cad1) != strlen(cad2)) {
+        return false;
+    }
 
+    //generando copias
+    char copia1[50], copia2[50];
+
+    //usamos la funcion strcpy();
+    strcpy(copia1, cad1);
+    strcpy(copia2, cad2);
+
+    ordenaCadena(copia1);
+    ordenaCadena(copia2);
+
+    char *p1 = copia1;
+    char *p2 = copia2;
+
+    while (*p1 != '\0' && *p2 != '\0') {
+        if (*p1 != *p2) {
+            return false;
+        }
+        p1++;
+        p2++;
+    }
+
+    return true;
 }
 
 int main(){
+
+    char cad1[]="oir";
+    char cad2[]="rio";
+
+    if (esAnagrama(cad1, cad2)) {
+        cout << cad1 << " y  " << cad2 << " son anagramas " <<endl;
+    } else {
+        cout << "no son anagramas" <<endl;
+    }
 
     return 0;
 }
