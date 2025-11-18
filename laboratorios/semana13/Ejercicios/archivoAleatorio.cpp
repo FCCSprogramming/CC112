6ccfaa04efd34886cdde5b96bb3fe819e98061b3
cc112FC <achulluncuyr@uni.edu.pe>
2025-11-18 12:21:35 -0500

    Ejercicios sesión 13 - Archivos

---
+++ laboratorios/semana13/Ejercicios/archivoAleatorio.cpp
@@ -0,0 +1,38 @@
+﻿/*
+Implementar un programa para guardar
+un conjunto de estudiantes en un archivo binario de registros
+de tamaño fijo, y luego modificar directamente el estudiante
+número k, sin leer ni reescribir todo el archivo.
+(Use archivos de acceso aleatorio)
+*/
+
+
+
+
+
+
+
+
+
+/*
+Ejercicio
+Crear un archivo binario llamado "empleados.dat" que almacene registros de tamaño fijo de empleados.
+Cada empleado debe tener la siguiente estructura:
+struct Empleado {
+    char nombre[30];
+    int edad;
+    float sueldo;
+};
+
+
+Valide que el índice no sea mayor al número de empleados.
+No reescribir todo el archivo para modificar un registro (acceso directo obligatorio).
+
+Implemente funciones  y presente la salida como un menú interactivo
+1. Agregar empleado
+2. Mostrar empleado por índice
+3. Modificar empleado por índice
+4. Mostrar número total de empleados
+5. Salir
+
+*/
---
+++ laboratorios/semana13/Ejercicios/archivoBinario.cpp
@@ -0,0 +1,54 @@
+﻿/*
+Ejemplo de archivo binario
+Aprenderemos a:
+- usar fstream con los modos ios::binary, ios::in, ios::out, ios::app.
+- usar write() y read() en lugar de << o >>.
+- guardar y recuperar estructuras.
+
+
+
+Implementar un programa para almacenar una lista de estudiantes con:
+nombre (máx 20 caracteres),
+código (entero),
+nota (float).
+
+Use un archivo binario llamado "estudiantes.dat".
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Ejercicio
+Crea un archivo binario llamado "productos.dat" que almacene:
+- nombre del producto (char[20])
+- código (int)
+- precio (float)
+- stock (int)
+
+Implementar:
+- Agregar producto
+- Mostrar productos
+- Buscar producto por código
+- Actualizar el stock de un producto (lectura/escritura binaria)
+*/
+
+
